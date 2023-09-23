0bee1b718f8fda5ef156945a2aa75b43788c7037
Andres <andresf150@gmail.com>
2023-09-22 20:33:53 -0600

    instanciando query

--- src/index.js
+++ src/index.js
@@ -3,10 +3,15 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 
+import { QueryClient, QueryClientProvider } from 'react-query';
+
+const queryClient = new QueryClient;
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-
+<QueryClientProvider client={queryClient}>
     <App />
+</QueryClientProvider>
   
 );
 
