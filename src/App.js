ee91fdd7a8c1c5799939c690841b688162274734
Andres <andresf150@gmail.com>
2023-09-22 20:50:05 -0600

    Configurando rutas

--- src/App.js
+++ src/App.js
@@ -1,8 +1,22 @@
+import {RouterProvider, createBrowserRouter} from "react-router-dom";
+import Show from "./componenetes/Show";
+import Details from "./componenetes/Details";
+
+const router = createBrowserRouter([{
+  path: "/",
+  element: <Show /> 
+},
+{
+  path: "products/:id",
+  element: <Details />
+}
+])
 
 function App() {
   return (
    <>
   <h1 className="text-2xl font-extrabold leading-none tracking-tight text-gray-900 md:text-5xl lg:text-4xl text-center">React Query</h1>
+  <RouterProvider router={router}/>
    </>
   );
 }
