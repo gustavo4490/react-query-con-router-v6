004ba18feeb202fb4b2978acfa4ba2a759768f7c
Andres <andresf150@gmail.com>
2023-09-24 12:55:48 -0600

    Mostrar detalles

--- src/componenetes/Details.jsx
+++ src/componenetes/Details.jsx
@@ -1,8 +1,45 @@
-import React from 'react'
+import React from 'react';
+import { useQuery } from 'react-query';
+import { Link, useParams } from 'react-router-dom';
+import { getById } from '../Utilitis/api';
 
 const Details = () => {
+  const urlParams = useParams();
+  const productId = urlParams.id;
+  const { data, error, isError, isLoading } = useQuery(['products', productId], () => getById(productId));
+  const divStyle = {
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    height: '100vh',
+    with: '70%',
+  };
+
+  if (isError) {
+    return <span>Error {error.message}</span>
+  }
+
+  if (isLoading) {
+    return <span>cargando....</span>
+  }
+
   return (
-    <div>Details</div>
+    <div style={divStyle}>
+
+      <div class="max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+
+        <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">El id es: {productId}</h5>
+
+        <p className="mb-3 font-normal text-gray-700 dark:text-gray-400"> {data.category}</p>
+        <Link to="/" className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+          Back
+          <svg class="w-3.5 h-3.5 ml-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
+            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9" />
+          </svg>
+        </Link>
+      </div>
+
+    </div>
   )
 }
 
