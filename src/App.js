658c74b3d014c66c52a6066373b18b7dcfee2d29
Andres <andresf150@gmail.com>
2023-09-29 16:04:39 -0600

    Agregar navegacion

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import {RouterProvider, createBrowserRouter} from "react-router-dom";
 import Show from "./componenetes/Show";
 import Details from "./componenetes/Details";
+import Nav from "./componenetes/nav"
 
 const router = createBrowserRouter([{
   path: "/",
@@ -15,6 +16,7 @@ const router = createBrowserRouter([{
 function App() {
   return (
    <>
+  <Nav/>
   <h1 className="text-2xl font-extrabold leading-none tracking-tight text-gray-900 md:text-5xl lg:text-4xl text-center">React Query</h1>
   <RouterProvider router={router}/>
    </>
